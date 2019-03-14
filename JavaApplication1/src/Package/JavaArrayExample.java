4c7a4f018a16217195caee5a56dcbfe13c99602b
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-03-13 20:34:25 -0600

    1

---
+++ JavaApplication1/src/Package/JavaArrayExample.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+package Package;
+
+
+import java.io.*;
+/**
+ *
+ * @author Alriosa
+ */
+public class JavaArrayExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String[] studentList = new String[4]; //Aqui colocar el numero en real de la posicion
+        
+        studentList[0] = "Juana";
+        studentList[1] = "Pedro";
+        studentList[2] = "Jose";
+        studentList[3] = "Freddy122!"; 
+        
+        System.out.println(studentList[0]);
+        System.out.println(studentList[1]);
+        System.out.println(studentList[2]);
+        System.out.println(studentList[3]);
+                                
+        
+        
+        
+    }
+    
+}
